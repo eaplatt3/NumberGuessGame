93630920e731ed5093e35aabf42066eb6a5c017e
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-02-26 07:56:36 -0500

    Initial commit

---
+++ NumberGuessGame.pl
@@ -0,0 +1,18 @@
+####################################
+# Earl Platt III
+# 2/26/2019
+# BCS 316 - Perl Programming
+# Number Guessing Game
+# Input - User Enters Number 
+#         Guess
+# Output - Display if User won
+#          How Far off User is 
+####################################
+
+use strict;
+use warning; 
+
+$randNum;
+$userGuess = <STDIN>;
+
+rand($randNum);
